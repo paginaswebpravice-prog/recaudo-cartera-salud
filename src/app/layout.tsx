de31654f06544ce26c1c30d66ea3c20da775a3f4
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-02-26 10:41:35 -0500

    Updates Google Tag Manager & Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,29 +52,19 @@ export default function RootLayout({
   return (
     <html lang="es-CO">
       <head>
-        <Script id="gtm-script" strategy="afterInteractive">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2QR8XBPWXX"
+          strategy="afterInteractive"
+        >
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id=GTM-WTP2T8TM'+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WTP2T8TM');
-          `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-2QR8XBPWXX');
+  `}
         </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WTP2T8TM"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         {children}
         <Footer />
       </body>
