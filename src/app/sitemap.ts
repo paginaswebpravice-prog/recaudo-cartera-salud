d4b9cdcd77324dbbd7bf03abc3335d3db9bb4df8
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-03-02 10:40:01 -0500

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function SiteMap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.recaudocarteraipsyeps.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.recaudocarteraipsyeps.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
