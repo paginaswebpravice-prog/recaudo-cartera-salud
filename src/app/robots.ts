5016bcf63c44c098d2f366e7525eceb1d21597f6
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2026-05-22 16:57:42 -0500

    Fix robots.ts archive

---
+++ src/app/robots.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+
+        allow: "/",
+
+        disallow: [
+          "/_next/",
+          "/api/",
+          "/*.woff2$",
+          "/*.woff$",
+          "/*.ttf$",
+          "/*.ico$",
+        ],
+      },
+    ],
+
+    sitemap: "https://www.recaudocarteraipsyeps.com/sitemap.xml",
+  };
+}
